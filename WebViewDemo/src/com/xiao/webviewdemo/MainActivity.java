7ea599567f153e396bf44a0d50de057194b6ead2
xiao098144 <xiao098144@users.noreply.github.com>
2014-12-29 18:04:52 +0800

    webview

---
+++ WebViewDemo/src/com/xiao/webviewdemo/MainActivity.java
@@ -0,0 +1,147 @@
+package com.xiao.webviewdemo;
+
+import java.util.Map;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebStorage;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+	WebView wv;
+	private String url;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		wv = (WebView) findViewById(R.id.wv);
+		wv.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+		
+		WebSettings settings = wv.getSettings();
+		settings.setAppCachePath(getFilesDir().getAbsolutePath());
+		settings.setAppCacheEnabled(true);
+		
+//		settings.setAppCacheMaxSize(10);
+		
+		
+//		settings.setBuiltInZoomControls(true);
+		
+		settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+		
+		settings.setDatabaseEnabled(true);
+		settings.setDefaultTextEncodingName("UTF-8");
+		/**		使用缓存 则 必须打开*/
+		settings.setDomStorageEnabled(true);
+		
+		
+//		settings.setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
+//		settings.setLayoutAlgorithm(LayoutAlgorithm.NORMAL);
+		settings.setLoadWithOverviewMode(true);
+		settings.setLoadsImagesAutomatically(true);
+		
+		settings.setNeedInitialFocus(true);
+		
+		settings.setBlockNetworkImage(false);
+		
+//		settings.setUseWideViewPort(true); 
+		
+//		settings.setSupportZoom(true);
+		
+        settings.setJavaScriptEnabled(false);
+        settings.setJavaScriptCanOpenWindowsAutomatically(false);
+        
+        wv.setWebChromeClient(new AppCacheWebChromeClient());
+		
+        wv.setWebViewClient(new WebViewClient(){
+        	
+        	@Override
+        	public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        		view.loadUrl(url);
+        		return true;
+        	}
+        	
+        	@Override
+        	public void onPageFinished(WebView view, String url) {
+        		super.onPageFinished(view, url);
+        		
+        	}
+        	
+        	 @Override
+             public void onLoadResource(WebView pWebView, String pUrl) {
+                 Log.e("APPCACHE", "onLoadResource: " + pUrl);
+             }
+
+             @SuppressWarnings("rawtypes")
+			@Override
+             public void onPageStarted(WebView pView, String pUrl, Bitmap pBit) {
+                 Log.e(" 资讯详情 ","onPageStarted: " + pUrl);
+
+                 // OK, we're on the correct thread here for callbacks
+                 // If not (such as in various parts of the Activity lifecycle) these WebStorage.* methods will never work
+				WebStorage.getInstance().getOrigins(new ValueCallback<Map> () {
+                     @SuppressLint("NewApi")
+					@Override
+                     public void onReceiveValue(Map map) {
+                         for(Object key : map.keySet()) {
+                             
+                             if(Build.VERSION.SDK_INT >= 11) {
+                                 WebStorage.Origin origin = (WebStorage.Origin) map.get(key);
+                                 Log.e(" 资讯详情 ", String.format("Origin: %s Quota: %s Usage: %s",
+                                         origin.getOrigin(), origin.getQuota(), origin.getUsage()));
+
+                             } else {
+                                 Log.e(" 资讯详情 ", "Key: " + key + " Value: " + map.get(key));
+                             }
+                         }
+                     }
+                 });
+
+                 // This should blow away this individual origin's manifests
+                 WebStorage.getInstance().deleteOrigin("http://mp.weixin.qq.com");
+
+                 // This should blow away everything, but does not on disk
+                 WebStorage.getInstance().deleteAllData();
+
+             }
+        });
+        wv.loadUrl(url);
+
+	}
+
+	private class AppCacheWebChromeClient extends WebChromeClient {
+
+        @SuppressWarnings("deprecation")
+		@Override
+        public void onReachedMaxAppCacheSize(long spaceNeeded, long totalUsedQuota, WebStorage.QuotaUpdater quotaUpdater) {
+            Log.e("APPCACHE", "onReachedMaxAppCacheSize reached, increasing space: " + spaceNeeded);
+            quotaUpdater.updateQuota(spaceNeeded * 2);
+        }
+        
+//        @Override
+//        public void onProgressChanged(WebView view, int newProgress) {
+//        	super.onProgressChanged(view, newProgress);
+//        	setTitle("正在加载中。。。");
+//        	setProgress(newProgress*100);
+//        }
+    }
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}

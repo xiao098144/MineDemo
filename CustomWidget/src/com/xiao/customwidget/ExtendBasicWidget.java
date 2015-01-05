d3e4ccdc279423f8932b6a949770adb3c6599fc2
xiao <xiao098144@users.noreply.github.com>
2015-01-05 23:00:30 +0800

    自定义控件 1、创建项目2015-1-5 22:59:21

---
+++ CustomWidget/src/com/xiao/customwidget/ExtendBasicWidget.java
@@ -0,0 +1,14 @@
+package com.xiao.customwidget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExtendBasicWidget extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.act_extendwidget);
+	}
+	
+}
